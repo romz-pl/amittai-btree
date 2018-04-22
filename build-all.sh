cd1ddfbec92f5d5e7176ba1ed83960a59e7821ed
Zbigniew Romanowski <romz@wp.pl>
2018-04-22 16:33:52 +0200

    Add test framework

--- build-all.sh
+++ build-all.sh
@@ -11,8 +11,8 @@ cd build || exit 1
 # Run cmake
 #
 
-# cmake -DBUILD_GTEST=ON -DBUILD_GMOCK=OFF -DCMAKE_BUILD_TYPE=Debug -Dbuild_tests=ON .. || exit 1
-cmake .. || exit 1
+cmake -DBUILD_GTEST=ON -DBUILD_GMOCK=OFF -DCMAKE_BUILD_TYPE=Debug .. || exit 1
+#cmake .. || exit 1
 make -j4 || exit 1
 
 
