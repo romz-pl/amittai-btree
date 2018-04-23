d25f3328e1de10054f6c83f361cc4a8d51d6982b
Zbigniew Romanowski <romz@wp.pl>
2018-04-23 20:45:17 +0200

    Move record printing into Printer class

--- src/Record.cpp
+++ src/Record.cpp
@@ -1,5 +1,3 @@
-#include <sstream>
-#include "Definitions.hpp"
 #include "Record.hpp"
 
 Record::Record( ValueType value )
@@ -13,9 +11,4 @@ ValueType Record::value() const
     return m_value;
 }
 
-std::string Record::to_string() const
-{
-    std::ostringstream oss;
-    oss << m_value;
-    return oss.str();
-}
+
