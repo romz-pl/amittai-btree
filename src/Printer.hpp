43762eb775ba169b97a7e10560fc9e4165ec7139
Zbigniew Romanowski <romz@wp.pl>
2018-04-23 16:58:20 +0200

    Add preprocesor ifdef for header files

--- src/Printer.hpp
+++ src/Printer.hpp
@@ -1,23 +1,5 @@
-//
-//  Printer.hpp
-//  BPlusTree.2a
-//
-//  Created by Amittai Aviram on 6/13/16.
-//  Copyright © 2016 Amittai Aviram. All rights reserved.
-//
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
-//
-//  http://www.apache.org/licenses/LICENSE-2.0
-//
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
-
-#pragma once
+#ifndef ROMZ_AMITTAI_BTREE_PRINTER_H
+#define ROMZ_AMITTAI_BTREE_PRINTER_H
 
 #include <queue>
 
@@ -37,3 +19,5 @@ private:
     void printCurrentRank(std::queue<Node*>* aCurrentRank, std::queue<Node*>* aNextRank) const;
     bool fVerbose;
 };
+
+#endif
