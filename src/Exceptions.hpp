367bf89a51f5ad26ddc13ffce08ac037a550b72b
Zbigniew Romanowski <romz@wp.pl>
2018-04-23 17:46:54 +0200

    Change names in class Exceptions

--- src/Exceptions.hpp
+++ src/Exceptions.hpp
@@ -9,29 +9,29 @@
 class LeafNotFoundException : public std::exception
 {
 public:
-    explicit LeafNotFoundException(KeyType);
+    explicit LeafNotFoundException( KeyType key );
     const char* what() const noexcept override;
 private:
-    KeyType fKey;
+    KeyType m_key;
 };
 
 class NodeNotFoundException : public std::exception
 {
 public:
-    explicit NodeNotFoundException(std::string, std::string);
+    explicit NodeNotFoundException( std::string searched_node, std::string containing_node );
     const char* what() const noexcept override;
 private:
-    std::string fSearchedNode;
-    std::string fContainingNode;
+    std::string m_searched_node;
+    std::string m_containing_node;
 };
 
 class RecordNotFoundException : public std::exception
 {
 public:
-    explicit RecordNotFoundException(KeyType);
+    explicit RecordNotFoundException( KeyType key );
     const char* what() const noexcept override;
 private:
-    KeyType fKey;
+    KeyType m_key;
 };
 
 #endif
