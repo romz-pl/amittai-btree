6e5081cb3b16361889f1411b2a33049f5bf53e8a
Zbigniew Romanowski <romz@wp.pl>
2018-04-23 17:39:40 +0200

    Change names in class Record

--- src/Record.cpp
+++ src/Record.cpp
@@ -1,39 +1,22 @@
-//
-//  Record.cpp
-//  BPlusTree.2a
-//
-//  Created by Amittai Aviram on 6/10/16.
-//  Copyright © 2016 Amittai Aviram. All rights reserved.
-//
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
-//
-//  http://www.apache.org/licenses/LICENSE-2.0
-//
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
-
 #include <string>
 #include <sstream>
 #include "Definitions.hpp"
 #include "Record.hpp"
 
-Record::Record(ValueType aValue)
-: fValue(aValue)
-{}
+Record::Record( ValueType value )
+    : m_value( value )
+{
+
+}
 
 ValueType Record::value() const
 {
-    return fValue;
+    return m_value;
 }
 
-std::string Record::toString() const
+std::string Record::to_string() const
 {
     std::ostringstream oss;
-    oss << fValue;
+    oss << m_value;
     return oss.str();
 }
