69ce698d2aac494cfdffb43ddd55c1849a9b9635
Zbigniew Romanowski <romz@wp.pl>
2018-04-24 20:57:33 +0200

    Add missing files to repository

---
+++ src/InternalElt.h
@@ -0,0 +1,20 @@
+#ifndef ROMZ_AMITTAI_BTREE_INTERNALELT_H
+#define ROMZ_AMITTAI_BTREE_INTERNALELT_H
+
+#include "KeyType.h"
+#include "Node.hpp"
+
+class InternalElt
+{
+public:
+    InternalElt( KeyType key, Node* node );
+    ~InternalElt() = default;
+
+public:
+    KeyType m_key;
+
+    Node* m_node;
+};
+
+
+#endif
---
+++ src/LeafElt.cpp
@@ -0,0 +1,8 @@
+#include "LeafElt.h"
+
+LeafElt::LeafElt( KeyType key, Record* record )
+    : m_key( key )
+    , m_record( record )
+{
+
+}
